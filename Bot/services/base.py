aea389fd917772c395791aea61757c315cee5d81
lamauspex <lamauspex@yandex.ru>
2026-01-27 22:49:56 +0300

    функционал перенесен в сервис

--- Bot/services/base.py
+++ Bot/services/base.py
@@ -1,28 +1,6 @@
-"""Базовый сервис"""
+""" Базовый сервис """
 
-from abc import ABC, abstractmethod
-from typing import Generic, TypeVar
-
-T = TypeVar('T')
-
-
-class AbstractRepository(ABC, Generic[T]):
-    """Абстрактный репозиторий для работы с данными"""
-
-    @abstractmethod
-    def get_all(self) -> T:
-        """Получить все данные"""
-        pass
-
-    @abstractmethod
-    def find_by_id(self, id_value: str):
-        """Найти по ID"""
-        pass
-
-    @abstractmethod
-    def find_by_field(self, field: str, value: str):
-        """Найти по полю"""
-        pass
+from abc import ABC
 
 
 class AbstractService(ABC):
